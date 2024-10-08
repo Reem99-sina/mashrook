0faa5cff5e2627e301de10db00e89541a8aaa50a
Reem99-sina <reemsina2@gmail.com>
2024-10-08 15:01:22 +0300

    well edit verify

--- src/app/verify/[email]/page.tsx
+++ src/app/verify/[email]/page.tsx
@@ -5,10 +5,10 @@ import { CloseButton, InfoOutLine, MashrookLogo } from "@/app/assets/svg";
 import { TextInput } from "../../components/shared/text-input.component";
 import { Button } from "../../components/shared/button.component";
 import { useParams } from "next/navigation";
-import { useState, useEffect,useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { AppDispatch, RootState } from "@/redux/store";
 import { useDispatch, useSelector } from "react-redux";
-import { verifyRequest,resendCodeRequest } from "@/redux/features/vierfySlice";
+import { verifyRequest, resendCodeRequest } from "@/redux/features/vierfySlice";
 import toast from "react-hot-toast";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import Cookie from "js-cookie"
@@ -17,27 +17,27 @@ import useFcmToken from '@/utils/hooks/useFcmToken';
 const Verify: React.FC = () => {
   const router = useParams();
   const links = useRouter();
-  const inputRefs = useRef<(HTMLInputElement|null)[]>([]);  
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
   const { email } = router;
   const [code, setCode] = useState(Array(4).fill(""));
-  const { fcmToken,notificationPermissionStatus } = useFcmToken();
+  const { fcmToken, notificationPermissionStatus } = useFcmToken();
   const [time, settime] = useState(60);
   const [canResend, setCanResend] = useState(false);
   let dispatch = useDispatch<AppDispatch>();
   let { loading, message, data } = useSelector<RootState>(
     (state) => state.verify
   ) as { loading: boolean; message: string; data: any };
-  const handleChange = (value: string, index: number) => { 
+  const handleChange = (value: string, index: number) => {
 
-            // Move to the next input if the current input is filled  
-            if (value.length === 1 && index < inputRefs.current.length - 1) {  
-                inputRefs?.current[index + 1]?.focus();  
-            }  
+    // Move to the next input if the current input is filled  
+    if (value.length === 1 && index < inputRefs.current.length - 1) {
+      inputRefs?.current[index + 1]?.focus();
+    }
 
-            // Move to the previous input if the current input is empty  
-            if (value.length === 0 && index > 0) {  
-                inputRefs?.current[index - 1]?.focus();  
-            }  
+    // Move to the previous input if the current input is empty  
+    if (value.length === 0 && index > 0) {
+      inputRefs?.current[index - 1]?.focus();
+    }
     const newCode = [...code];
     newCode[index] = value;
     setCode(newCode);
@@ -49,14 +49,14 @@ const Verify: React.FC = () => {
         verifyRequest({
           email: String(email)?.replace("%40", "@"),
           code: code.join(""),
-          device_token:fcmToken
+          device_token: fcmToken
         })
-      ).then((res:any)=>{
-        if(res.payload.message&&!res.payload.status){
+      ).then((res: any) => {
+        if (res.payload.message && !res.payload.status) {
           toast.success(res.payload.message);
           Cookie.set("token", res.payload?.data?.token);
           links.push(`/`);
-        }else if(res.payload.status){
+        } else if (res.payload.status) {
           toast.error(res.payload.message);
         }
       })
@@ -64,18 +64,18 @@ const Verify: React.FC = () => {
       toast.error("you need email and code");
     }
   };
-  const onResend=()=>{
-    dispatch(resendCodeRequest({email:String(email)?.replace("%40", "@")})).then((res:any)=>{
-      if(res.payload.message&&!res.payload.status){
+  const onResend = () => {
+    dispatch(resendCodeRequest({ email: String(email)?.replace("%40", "@") })).then((res: any) => {
+      if (res.payload.message && !res.payload.status) {
         toast.success(res.payload.message);
-      }else if(res.payload.status){
+      } else if (res.payload.status) {
         toast.error(res.payload.message);
       }
     })
   }
- 
+
   useEffect(() => {
-    let interval:NodeJS.Timeout;
+    let interval: NodeJS.Timeout;
     if (time > 0) {
       interval = setInterval(() => {
         settime((prevTimer) => prevTimer - 1);
@@ -104,13 +104,13 @@ const Verify: React.FC = () => {
               <br />
               الرجاء قم بإدخال رمز التحقق لإنشاء حسابك
             </p>
-            
+
             <div className="flex justify-center mb-4">
               {code.map((digit, index) => (
                 <input
                   key={index}
-                  type="text"
-                  ref={(el) => {inputRefs.current[index] = el}} 
+                  type="number"
+                  ref={(el) => { inputRefs.current[index] = el }}
                   maxLength={1}
                   pattern="\d{4}"
                   className="w-12 h-12 m-1 text-center border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -121,7 +121,7 @@ const Verify: React.FC = () => {
             </div>
             <p className="text-center text-sm text-gray-500 mb-4">
               لم يصلك الرمز؟{" "}
-              <button className={`${time!=0?"text-gray-500":"text-[#98CC5D]"}`} onClick={onResend} type="button" disabled={time!=0}>
+              <button className={`${time != 0 ? "text-gray-500" : "text-[#98CC5D]"}`} onClick={onResend} type="button" disabled={time != 0}>
                 إعادة إرسال الرمز خلال {time} ثانية
               </button>
             </p>
@@ -134,19 +134,19 @@ const Verify: React.FC = () => {
             <InfoOutLine />
           </div>
           <div>
-            {loading?<button
+            {loading ? <button
               className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-[#3B73B9] border border-transparent rounded-md group  focus:outline-none focus:ring-2 focus:ring-offset-2 "
               disabled={loading}
-            ><AiOutlineLoading3Quarters className="rotate-90 text-gray-500"/>
+            ><AiOutlineLoading3Quarters className="rotate-90 text-gray-500" />
             </button>
-            
-            : <Button
-              text="تسجيل جديد"
-              className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-[#3B73B9] border border-transparent rounded-md group  focus:outline-none focus:ring-2 focus:ring-offset-2 "
-              type="submit"
-              onClick={onSubmit}
-            />}
-           
+
+              : <Button
+                text="تسجيل جديد"
+                className="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-[#3B73B9] border border-transparent rounded-md group  focus:outline-none focus:ring-2 focus:ring-offset-2 "
+                type="submit"
+                onClick={onSubmit}
+              />}
+
           </div>
         </form>
       </div>
